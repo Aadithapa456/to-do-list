860365a1ce8cc7c9ad2f69ffd94ffe8c29f6e66a
Aaditya <aadityathapa977@gmail.com>
2024-10-04 09:08:48 +0545

    Added date as id

--- script.js
+++ script.js
@@ -24,62 +24,66 @@
 // Getting the user input
 let userInput = document.querySelector("#user-input");
 let addBtn = document.querySelector(".add");
-// Targeting DOM Elements
 let itemContainer = document.querySelector(".main");
-let idForListItems = 1;
-let myToDoTask = [];
+let myToDoTask = JSON.parse(localStorage.getItem("tasks")) || [];
+
 function addItems() {
-   let userInputValue = userInput.value;
-   if (userInputValue == "") {
+   let userInputValue = userInput.value.trim();
+   if (userInputValue === "") {
       alert("Enter something");
    } else {
-      createItems(userInputValue);
+      let taskId = Date.now(); // Unique ID using timestamp
+      let taskObj = { id: taskId, text: userInputValue };
+      createItems(taskObj);
+      myToDoTask.push(taskObj);
+      storeInBrowser();
    }
    userInput.value = "";
 }
+
+function createItems(taskObj) {
+   let newList = document.createElement("div");
+   newList.className = "todo-items";
+   newList.dataset.id = taskObj.id;
+   newList.innerHTML = `
+      <div class="text-section">
+         <div class="item item-1">
+            <div class="list-text">
+               <input type="checkbox" id="list-${taskObj.id}">
+               <label for="list-${taskObj.id}">${taskObj.text}</label>
+            </div>
+         </div>
+      </div>
+      <div class="item-delete">
+         <button class="remove-btn">
+            <i class="fa-solid fa-trash"></i>
+         </button>
+      </div>
+   `;
+   itemContainer.append(newList);
+   // Task Deletion
+   let removeBtn = newList.querySelector(".remove-btn");
+   removeBtn.addEventListener("click", () => {
+      newList.remove();
+      myToDoTask = myToDoTask.filter((task) => task.id !== taskObj.id); // Remove by ID
+      storeInBrowser();
+   });
+}
+
+function storeInBrowser() {
+   localStorage.setItem("tasks", JSON.stringify(myToDoTask));
+}
+
+function retrieveData() {
+   myToDoTask.forEach((task) => createItems(task));
+}
+
 addBtn.addEventListener("click", addItems);
 userInput.addEventListener("keydown", (e) => {
-   if (e.key == "Enter") {
+   if (e.key === "Enter") {
       e.preventDefault();
       addItems();
    }
 });
 
-function createItems(task) {
-   let newList = document.createElement("div");
-   newList.className = "todo-items";
-   newList.innerHTML = `
-      <div class="text-section">
-      <div class="item item-1">
-      <div class="list-text">
-      <input type="checkbox" id="list-${idForListItems}">
-      <label for="list-${idForListItems}">${task}</label>
-      </div>  
-      </div>
-      </div>
-      <div class="item-delete"><button class="remove-btn"><i class="fa-solid fa-trash"></i></button></div>
-      `;
-   itemContainer.append(newList);
-   let removeBtn = newList.querySelector(".remove-btn");
-   removeBtn.addEventListener("click", () => {
-      newList.remove();
-      let taskIndex = myToDoTask.indexOf(task);
-      myToDoTask.splice(taskIndex, 1);
-      storeInBrowser(myToDoTask); // Updates the data after deletion
-   });
-   myToDoTask.push(task);
-   storeInBrowser(myToDoTask);
-   idForListItems++;
-}
-function storeInBrowser(myToDoTask) {
-   localStorage.setItem("tasks", JSON.stringify(myToDoTask));
-}
-function retrieveData() {
-   let storedTasks = JSON.parse(localStorage.getItem("tasks"));
-   if (storedTasks) {
-      storedTasks.forEach((task) => {
-         createItems(task);
-      });
-   }
-}
 document.addEventListener("DOMContentLoaded", retrieveData);
